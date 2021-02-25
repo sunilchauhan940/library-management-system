9117dc9eadc420d034e62b22d275ac6d9a125314
hitesh7865 <76428717+hitesh7865@users.noreply.github.com>
2021-02-25 15:52:09 +0530

    many changes

---
+++ assets/view/main.php
@@ -0,0 +1,79 @@
+<div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">Dashboard</h1>
+
+                        <div class="card mb-4">
+                            <div class="card-header">
+                            <!-- <i class="fas fa-table mr-1"></i> -->
+                                <b>Add Admin</b>
+                            </div>
+                            <div class="card-body">
+                                <form>
+                                    <div class="form-group row">
+                                        <label class="col-xl-3">Name<i class="text-danger">*</i></label>
+                                        <div class="form-group">
+                                            <div class="col-xs-7">
+                                                <input name="name" type="text" class="form-control" id="name" placeholder="Enter Name..." value="" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-xl-3">Email<i class="text-danger">*</i></label>
+                                        <div class="form-group">
+                                            <div class="col-xs-7">
+                                                <input name="email" type="text" class="form-control" id="" placeholder="Enter Email..." value="" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-xl-3">Mobile<i class="text-danger">*</i></label>
+                                        <div class="form-group">
+                                            <div class="col-xs-7">
+                                                <input name="mobile" type="text" class="form-control" id="" placeholder="Enter Mobile..." value="" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-xl-3">BirthDate<i class="text-danger">*</i></label>
+                                        <div class="form-group">
+                                            <div class="col-xs-7">
+                                                <input name="birth_date" type="date" class="form-control" id="" placeholder="" value="" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-xl-3">Gender<i class="text-danger">*</i></label>
+                                        <div class="form-group">
+                                            <div class="col-xs-7">
+                                                <select class="form-control" name="gender">
+                                                    <option value="male">MALE</option>
+                                                    <option value="female">FEMALE</option>
+                                                    <option value="other">OTHER</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-xl-3">Password<i class="text-danger">*</i></label>
+                                        <div class="form-group">
+                                            <div class="col-xs-7">
+                                                <input name="password" type="password" class="form-control" id="" placeholder="Enter Pasword..." value="" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <div class="col-xl-8">
+                                            <input type="submit" id="add" class="btn btn-lg btn-success" value="Save">
+                                            <input type="reset" class="btn btn-lg btn-danger" name="Reset"/>                                
+                                        </div>   
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <?php
+                    include "footer.php";
+                    ?>
+            </div>
