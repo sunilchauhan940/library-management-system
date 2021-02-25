4183400a7ee6836bb065ca3269506bb6d51c58fd
Sunil Chauhan <sunilchauhan940940@gmail.com>
2021-02-25 22:25:21 +0530

    Added Script to Insert New Entry in Database

--- adduser.php
+++ adduser.php
@@ -1,3 +1,32 @@
+<?php
+include 'connect.php';
+if (isset($_POST['submit'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $mobile = mysqli_real_escape_string($conn, $_POST['mobile']);
+    $gender = mysqli_real_escape_string($conn, $_POST['gender']);
+    $birth_date = mysqli_real_escape_string($conn, $_POST['birth_date']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    //$pass = md5($password);
+    $sql = "SELECT id FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows > 0) {
+        echo '<script type="text/javascript">alert("Email already used before");</script>';
+        exit();
+    } else {
+        $sql = "INSERT INTO `users` (`name`, `email`, `mobile`, `birth_date`, `address`, `gender`, `password`) VALUES ('$name', '$email',$mobile,'$birth_date','$address', '$gender','$password');";
+        if (mysqli_query($conn, $sql)) {
+            echo "<script>alert('Registered Successfully');</script>";
+            header('Location:index.php');
+        } else {
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+        }
+    }
+    mysqli_close($conn);
+}
+?>
+
 <?php include('header.php'); ?>
 <div class="container">
         <ol class="breadcrumb">
@@ -15,21 +44,15 @@
                     <div class="col-md-12">
                         <div class="form-group">
                             <label>Full Name</label>
-                            <input type="text" name="name" id="name" minlength="2" required="" class="form-control" placeholder="Full Name">
+                            <input type="text" name="name" id="name" required class="form-control" placeholder="Full Name">
                         </div>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-12">
                         <div class="form-group">
                             <label>Email</label>
-                            <input type="email" id="email_id" class="form-control" required="" name="email_id" placeholder="Email">
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Phone no</label>
-                            <input type="tel"  id="phone_no" class="form-control" required="" name="phone_no" placeholder="Phone no">
+                            <input type="email" id="email" class="form-control" required name="email" placeholder="Email">
                         </div>
                     </div>
                 </div>
@@ -37,13 +60,13 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" id="password" minlength="6" maxlength="16" name="password" required="" class="form-control" placeholder="Password">
+                            <input type="password" id="password" minlength="6" maxlength="16" name="password" required class="form-control" placeholder="Password">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label>Confirm Password</label>
-                            <input type="password" id="cpassword" minlength="6" maxlength="16" name="cpassword" required="" class="form-control" placeholder="Confirm Password">
+                            <label>Phone no</label>
+                            <input type="tel"  id="mobile" class="form-control" required name="mobile" placeholder="Phone no">
                         </div>
                     </div>
                 </div>
@@ -51,17 +74,25 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label>Gender :</label>
-                            <select name="gender" class="form-control" required="">
+                            <select name="gender" class="form-control" required>
                                 <option value="">Select Gender</option>
                                 <option value="Male">Male</option>
                                 <option value="Female">Female</option>
                             </select>
                         </div>
                     </div>
-                    <div class="col-md-6"-->
+                    <div class="col-md-6">
                         <div class="form-group">
                             <label>Birth Date</label>
-                            <input type="date" id="bdate" name="birth_date" class="form-control" placeholder="Birth Date">
+                            <input type="date" id="bdate" name="birth_date" class="form-control" placeholder="Birth Date" required>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea class="form-control" id="address" name="address" rows="2" required></textarea>
                         </div>
                     </div>
                 </div>
