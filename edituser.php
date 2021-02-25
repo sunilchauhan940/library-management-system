3681aac6bc54f24dc8844ad5b7d6244794f232ed
Sunil Chauhan <sunilchauhan940940@gmail.com>
2021-02-25 22:33:05 +0530

    Added Script to Update Existing Entry in Database

--- edituser.php
+++ edituser.php
@@ -1,3 +1,23 @@
+<?php
+include 'connect.php';
+$id = $_REQUEST['id'];
+if (isset($_POST['submit'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $mobile = mysqli_real_escape_string($conn, $_POST['mobile']);
+    $gender = mysqli_real_escape_string($conn, $_POST['gender']);
+    $birth_date = mysqli_real_escape_string($conn, $_POST['birth_date']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+
+    $update = "UPDATE `users` SET `name` = '$name', `email` = '$email', `mobile` = '$mobile', `birth_date` = '$birth_date', `address` = '$address', `gender` = '$gender' WHERE `users`.`id` = $id";
+    
+    if (mysqli_query($conn, $update)) {
+        header("Location:index.php");
+    } else {
+        echo "ERROR: Could not able to execute $update. " . mysqli_error($conn);
+    }
+}
+?>
 <?php include('header.php'); ?>
 <div class="container">
         <ol class="breadcrumb">
@@ -23,13 +43,13 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label>Email</label>
-                            <input type="email" id="email_id" class="form-control" required="" name="email_id" placeholder="Email">
+                            <input type="email" id="email" class="form-control" required name="email" placeholder="Email">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label>Phone no</label>
-                            <input type="tel"  id="phone_no" class="form-control" required="" name="phone_no" placeholder="Phone no">
+                            <input type="tel"  id="mobile" class="form-control" required name="mobile" placeholder="Phone no">
                         </div>
                     </div>
                 </div>
@@ -37,17 +57,25 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label>Gender :</label>
-                            <select name="gender" class="form-control" required="">
+                            <select name="gender" class="form-control" required>
                                 <option value="">Select Gender</option>
                                 <option value="Male">Male</option>
                                 <option value="Female">Female</option>
                             </select>
                         </div>
                     </div>
-                    <div class="col-md-6"-->
+                    <div class="col-md-6">
                         <div class="form-group">
                             <label>Birth Date</label>
-                            <input type="date" id="bdate" name="birth_date" class="form-control" placeholder="Birth Date">
+                            <input type="date" id="bdate" name="birth_date" class="form-control" placeholder="Birth Date" required>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea class="form-control" id="address" name="address" rows="2" required></textarea>
                         </div>
                     </div>
                 </div>
